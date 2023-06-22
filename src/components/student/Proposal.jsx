6ce2cad68ec4b279fd6d9050921c148c04596951
Rameesha Khan <rameeshakhan75@gmail.com>
2023-06-22 14:04:48 +0500

    Changes

--- src/components/student/Proposal.jsx
+++ src/components/student/Proposal.jsx
@@ -5,20 +5,6 @@ import { useNavigate } from "react-router-dom";
 
 const Proposal = () => {
   const navigate = useNavigate()
-  const acceptedRequestData = [
-    {
-      subject: "Math",
-      description:
-        "Mathematics is the study of numbers, shapes, and patterns. It encompasses various branches such as algebra, geometry, and calculus. Science, on the other hand, is the systematic study of the natural world through observation and experimentation, including disciplines like physics, chemistry, and biology.",
-      status: "accepted",
-    },
-    {
-      subject: "Science",
-      description:
-        "Sports refer to physical activities involving competitive games, exercises, and challenges. They promote physical fitness, teamwork, and skill development. Fitness encompasses a broader concept of overall health and well-being, including activities like exercise, nutrition, and lifestyle choices to maintain optimal physical and mental condition.",
-      status: "accepted",
-    },
-  ];
 
   const handleClassroomBtn = () => {
     navigate("/classroom")
@@ -28,12 +14,7 @@ const Proposal = () => {
     <div>
       <h4 className={styles.heading}> Proposals</h4>
       <div>
-        {acceptedRequestData.map((data) => (
-          <div className={styles.contaier} key={data.subject}>
-            <h5>{data.subject}</h5>
-            <button className={styles.button} onClick={handleClassroomBtn}>Go To Classroom</button>
-          </div>
-        ))}
+       Accepted Proposals
       </div>
     </div>
   );
